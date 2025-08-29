6d17226fae641d2dff511db73aea66414aa2f30b
himanshugithu <fanibharehimanshu@gmail.com>
2025-08-29 15:48:16 +0530

    add coap

---
+++ DHT11_COAP/GNU ARM v12.2.1 - Debug/autogen/sl_simple_led_instances.d
@@ -0,0 +1,19 @@
+autogen/sl_simple_led_instances.o: ../autogen/sl_simple_led_instances.c \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\driver\leddrv\inc\sl_simple_led.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\driver\leddrv\inc\sl_led.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_status.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\driver\gpio\inc\sl_gpio.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\service\device_manager\inc\sl_device_gpio.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_enum.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_code_classification.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sli_code_classification.h \
+ C:\Users\iamfa\SimplicityStudio\v5_workspace\DHT11_COAP\config/sl_simple_led_led0_config.h
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\driver\leddrv\inc\sl_simple_led.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\driver\leddrv\inc\sl_led.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_status.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\driver\gpio\inc\sl_gpio.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\service\device_manager\inc\sl_device_gpio.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_enum.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_code_classification.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sli_code_classification.h:
+C:\Users\iamfa\SimplicityStudio\v5_workspace\DHT11_COAP\config/sl_simple_led_led0_config.h:
---
+++ DHT11_COAP/GNU ARM v12.2.1 - Debug/main.d
@@ -0,0 +1,21 @@
+main.o: ../main.c \
+ C:\Users\iamfa\SimplicityStudio\v5_workspace\DHT11_COAP\autogen/sl_component_catalog.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\service\system\inc\sl_system_init.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_common.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_assert.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\cmsis\core\include\cmsis_compiler.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\cmsis\core\include\cmsis_gcc.h \
+ ../app.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\cmsis\rtos2\include\cmsis_os2.h \
+ c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\service\system\inc\sl_system_kernel.h \
+ ../app_init.h
+C:\Users\iamfa\SimplicityStudio\v5_workspace\DHT11_COAP\autogen/sl_component_catalog.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\service\system\inc\sl_system_init.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_common.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\common\inc\sl_assert.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\cmsis\core\include\cmsis_compiler.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\cmsis\core\include\cmsis_gcc.h:
+../app.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\cmsis\rtos2\include\cmsis_os2.h:
+c:\users\iamfa\simplicitystudio\sdks\simplicity_sdk\platform\service\system\inc\sl_system_kernel.h:
+../app_init.h:
